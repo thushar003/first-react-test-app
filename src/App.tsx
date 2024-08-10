346ef73f98b854854014d01d38fa7da5188ed930
thushar003 <thusharjoji2@gmail.com>
2024-08-10 17:58:05 -0400

    Created Alert Using code from Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import ListGroup from "./components/ListGroup";
-import { Fragment } from "react";
+// import ListGroup from "./components/ListGroup";
+// import { Fragment } from "react";
 
-function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// function App() {
+//   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   }
 
+//   return (
+//     <div>
+//       <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+//     </div>
+//   );
+//   // or return <div><Message /></div>;
+// }
+
+// export default App;
+
+import React from 'react'
+import Alert from './components/Alert';
+
+const App = () => {
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      <Alert text="Hello World"/>
     </div>
-  );
-  // or return <div><Message /></div>;
+  )
 }
 
 export default App;
